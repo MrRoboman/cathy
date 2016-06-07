1051ef10716f3c1240f885570277d89c8f13733e
MrRoboman <rkayson@gmail.com>
2016-06-07 09:38:16 -0700

    simple app complete

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('user connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(8080, function() {
